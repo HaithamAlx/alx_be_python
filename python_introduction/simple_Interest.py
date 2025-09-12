f84a9839e0a691724a04ec0bbe682cec42d37284
Haitham <hmohamed.r1123@gmail.com>
2025-09-11 22:10:52 -0700

    Week 3 project

---
+++ python_introduction/simple_Interest.py
@@ -0,0 +1,6 @@
+principal = 1000  # representing $1000 
+Rate = 0.05       # representing 5% annual interest rate
+time = 3          # representing 3 years
+
+Interest = principal * Rate * time
+print ("The simple interest is:", Interest)
