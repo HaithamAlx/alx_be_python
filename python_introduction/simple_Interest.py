f3fd5fc7ac7d527c3571c731d6f3de1ed6d0824b
Haitham <hmohamed.r1123@gmail.com>
2025-09-21 08:09:20 +0300

    Copy the file to control flow folder

--- python_introduction/simple_Interest.py
+++
@@ -1,6 +0,0 @@
-principal = 1000  # representing $1000 
-Rate = 0.05       # representing 5% annual interest rate
-time = 3          # representing 3 years
-
-Interest = principal * Rate * time
-print ("The simple interest is:", Interest)
