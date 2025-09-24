cac1c4a96329ecb52bb5cbbf3602a7269eaee9f2
Haitham <hmohamed.r1123@gmail.com>
2025-09-24 07:16:02 +0300

    fix range in multiplication script

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 
-for table in range(0, 11):
+for table in range(1, 11):
   print(f"{number} * {table} = {number * table}")
