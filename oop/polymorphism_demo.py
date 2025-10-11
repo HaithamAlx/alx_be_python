ac6e8a25e672ad674176f47fa05f76fb6de10406
Haitham <hmohamed.r1123@gmail.com>
2025-10-11 16:24:03 -0700

    3rd Task

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -25,12 +25,12 @@ class Circle(Shape):
     
 def main():
     shapes = [
-        Rectangle(10, 5),
+        Rectangle(10,5),
         Circle(7)
     ]
 
     for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+        print(f"The area of the {shape.__class__.__name__} is:{shape.area()}")
 
 if __name__ == "__main__":
     main()
