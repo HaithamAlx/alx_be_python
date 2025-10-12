4ffa3945fe6503324e351e7e66dbb2219e78e7d7
Haitham <hmohamed.r1123@gmail.com>
2025-10-11 17:39:10 -0700

    Fixed txt

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -14,7 +14,7 @@ def calculate_future_date(days_to_add):
 def main():
     display_current_datetime()
     try:
-        days = int(input("Enter the number of days to add to today's date: "))
+        days = int(input("Enter the number of days to add to the current date: "))
         calculate_future_date(days)
     except ValueError:
         print("Invalid input: Please enter an integer number of days, no partial days.")
